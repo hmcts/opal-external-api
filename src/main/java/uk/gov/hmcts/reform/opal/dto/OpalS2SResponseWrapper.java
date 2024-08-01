efa68d30bb8981d772495c61d5d9a1e6a9f7f2d0
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-08-01 15:05:53 +0100

    add constructors to allow for serialization

--- src/main/java/uk/gov/hmcts/reform/opal/dto/OpalS2SResponseWrapper.java
+++ src/main/java/uk/gov/hmcts/reform/opal/dto/OpalS2SResponseWrapper.java
@@ -1,11 +1,15 @@
 package uk.gov.hmcts.reform.opal.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class OpalS2SResponseWrapper {
 
     @JsonProperty("opalResponsePayload")
