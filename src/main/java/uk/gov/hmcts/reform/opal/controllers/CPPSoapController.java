9e1cb624ddccd3bb2378f3a2ec68429d5a8591bb
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-08-01 15:05:32 +0100

    call real service method not stubbed method

--- src/main/java/uk/gov/hmcts/reform/opal/controllers/CPPSoapController.java
+++ src/main/java/uk/gov/hmcts/reform/opal/controllers/CPPSoapController.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.opal.controllers;
 
 import jakarta.jws.WebService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.opal.CPPSoapGateway.CreateFineAccountsRequest;
 import uk.gov.hmcts.reform.opal.CPPSoapGateway.CreateFineAccountsResponse;
@@ -13,20 +14,21 @@ import uk.gov.hmcts.reform.opal.service.CreateFineAccountsService;
 @Service
 @WebService(serviceName = "CppGatewayService", portName = "CppGatewayPort", targetNamespace = "http://www.justice.gov.uk/magistrates/atcm/CPPSoapGateway", endpointInterface = "uk.gov.hmcts.reform.opal.CPPSoapGateway.ICppGatewayType")
 @RequiredArgsConstructor
-public class CPPSoapController implements ICppGatewayType {
+@Slf4j
+public class CppSoapController implements ICppGatewayType {
 
     private final CreateFineAccountsService createFineAccountsService;
 
     @Override
     public CreateFineAccountsResponse createFineAccounts(CreateFineAccountsRequest request) {
-       return createFineAccountsService.handleCreateFineAccountsRequestStub(request);
-   }
+        return createFineAccountsService.handleCreateFineAccountsRequest(request);
+    }
 
-   @Override
+    @Override
     public SearchResponseType searchType(SearchRequestType request) {
         //not yet implemented
-       SearchResponseType response = new SearchResponseType();
-       response.setErrorMessage("FAKE ERROR MESSAGE 2");
-       return response;
+        SearchResponseType response = new SearchResponseType();
+        response.setErrorMessage("ERROR MESSAGE");
+        return response;
     }
 }
