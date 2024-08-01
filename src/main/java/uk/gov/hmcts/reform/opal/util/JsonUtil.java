81924574bfc016d6c10395fed7649d884c0c211f
DeclanClarkeCGI <142809814+DeclanClarkeCGI@users.noreply.github.com>
2024-08-01 15:06:37 +0100

    new util method for json serialization

--- src/main/java/uk/gov/hmcts/reform/opal/util/JsonUtil.java
+++ src/main/java/uk/gov/hmcts/reform/opal/util/JsonUtil.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.opal.util;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonUtil {
+
     public static <T> T convertJsonToObject(String rawJson, Class<T> clzz) {
         try {
             // Create an ObjectMapper instance
@@ -12,6 +13,18 @@ public class JsonUtil {
             return objectMapper.readValue(rawJson, clzz);
         } catch (Exception e) {
             throw new RuntimeException("Failed to convert JSON to object", e);
+        }
+    }
+
+    public static String convertObjectToJson(Object object) {
+        try {
+            // Create an ObjectMapper instance
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // Serialize the object into a JSON string
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to convert object to JSON", e);
         }
     }
 }
